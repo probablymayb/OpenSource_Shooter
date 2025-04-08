088dc7ab6d0b4b7d701d94ed23a3c5c90ab0b438
정하은 <he3080@naver.com>
2025-04-09 01:15:33 +0900

    Feat: 인게임 씬에서 이용될 UI(Popup)들을 관리하는 컨트롤러 구현 - Tab키가 눌렸을 때 매칭 팝업 생성

---
+++ Assets/Scripts/Global Managers and Controllers/Manager/UI/UIController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    public float Progress { get; private set; }
+
+    private const string matchPopupName = "MatchingPopup";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameManager.Initialize();
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        GetPlayerInput();
+    }
+
+    void GetPlayerInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            var popup = GameManager.UI.IsPopupStack(matchPopupName);
+
+            if (popup != null)
+            {
+                GameManager.UI.HidePopup(popup);
+            }
+            else
+            {
+                GameManager.UI.ShowPopup(matchPopupName).Forget();
+            }
+        }
+    }
+}
