e5e3f10b4cb5c7367470c549814d0fa2a85d552a
정하은 <he3080@naver.com>
2025-05-08 18:01:33 +0900

    Feat: [IScenePhotonManager] 각 씬 별 포톤 매니저들은 인터페이스를 구현하도록 한다. - InitializeSceneObject() - UpdatePlayerAvatar(Player player)

---
+++ Assets/Scripts/Global Managers and Controllers/Manager/Interface/IScenePhotonManager.cs
@@ -0,0 +1,9 @@
+﻿using Photon.Realtime;
+
+public interface IScenePhotonManager
+{
+    void Initialize();
+    void Release();
+    void InitializeSceneObject();
+    void UpdatePlayerAvatar(Player player);
+}
