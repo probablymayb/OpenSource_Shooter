4909237627471bd45d7c03cce4900c76434c891f
정하은 <he3080@naver.com>
2025-07-03 23:08:53 +0900

    [Feat] PlayerStatusUIController: 내 Hp바와 상대 Hp바 UI를 처리하는 컨트롤러 추가

---
+++ Assets/Scripts/UI/PlayerStatusUIController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.PlayerLoop;
+using Photon.Pun;
+
+public class PlayerStatusUIController : MonoBehaviour
+{
+    private PhotonView _photonView;
+    
+    void Awake()
+    {
+        _photonView = GetComponentInParent<PhotonView>();
+    }
+
+    void Start()
+    {
+        if (_photonView == null) _photonView = GetComponentInParent<PhotonView>();
+
+        if (!_photonView.IsMine)
+        {
+            Debug.Log("[PlayerStatusUIController] 이 캐릭터는 내 것이 아님. UI 조정 시작.");
+
+            GameObject myAvatar = InGamePhotonManager.InGame.GetMyAvatar();
+            if (myAvatar != null)
+            {
+                var myUI = myAvatar.GetComponentInChildren<PlayerStatusUIController>();
+                if (myUI != null)
+                {
+                    float myX = myUI.GetComponent<RectTransform>().anchoredPosition.x;
+
+                    RectTransform rect = GetComponent<RectTransform>();
+                    Debug.Log(rect.transform.parent.name);
+                    rect.anchoredPosition = new Vector2(-myX, rect.anchoredPosition.y);
+
+                    Image img = GetComponentInChildren<Image>();
+                    if (img != null)
+                    {
+                        img.color = Color.red;
+                        Debug.Log("→ HP 바 색상 변경 완료");
+                    }
+
+                    Debug.Log($"→ 위치 조정 완료: x={-myX}");
+                }
+            }
+            else
+            {
+                Debug.LogWarning("→ 내 아바타를 찾지 못했습니다.");
+            }
+        }
+        else
+        {
+            Debug.Log("[PlayerStatusUIController] 이건 내 캐릭터입니다. UI 변경 없음.");
+        }
+    }
+
+}
