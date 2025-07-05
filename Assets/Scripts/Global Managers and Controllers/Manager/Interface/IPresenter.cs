be150b5de6f069a231c41727a6607075d285ac8b
정하은 <he3080@naver.com>
2025-07-05 23:30:04 +0900

    [Feat] IPresenter: Presenter들이 구현할 인터페이스 추가

---
+++ Assets/Scripts/Global Managers and Controllers/Manager/Interface/IPresenter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct Unit
+{
+    public static readonly Unit Default = new Unit();
+}
+
+public interface IPresenter
+{
+    void ShowView();
+    void HideView();
+    void TriggerEvent(Enum eventName);
+    void TriggerEvent<T>(Enum eventName, T param);
+}
