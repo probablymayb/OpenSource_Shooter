f2a8a0a96405e7e8e287a04a19fdc45df6ad67d0
정하은 <he3080@naver.com>
2025-07-05 23:32:08 +0900

    [Feat] MVP Base 구현 - PresenterBase: View 이벤트 등록/실행 구조 구현 (AddListener, TriggerEvent 기반) - ViewBase: UI 입력 요소와 이벤트 연결하는 AddUIListener 구조 구현 - ModelBase: DTO 기반 모델 변경 이벤트 등록/트리거 시스템 구현

---
+++ Assets/Scripts/Global Managers and Controllers/Manager/UI/Base/ModelBase.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// 데이터를 보관하고 상태를 관리하는 Model의 Base클래스
+/// 필요한 데이터들의 초기값 설정이 필수적이다.
+/// </summary>
+public abstract class ModelBase : MonoBehaviour
+{
+    private readonly UnityEvent _onModelChanged = new UnityEvent();
+
+    private readonly Dictionary<Enum, Action<object>> _onModelChangedDTO = new Dictionary<Enum, Action<object>>();
+
+    /// <summary>
+    /// Model의 초기화를 진행하는 메서드
+    /// </summary>
+    public virtual void Initialize() { }
+
+    /// <summary>
+    /// 모델 내 이벤트에 리스너를 추가하는 메서드
+    /// </summary>
+    /// <param name="listener">추가 할 리스너</param>
+    public void AddListener(UnityAction listener)
+    {
+        _onModelChanged.AddListener(listener);
+    }
+
+    /// <summary>
+    /// DTO 기반 특정 데이터 변경 이벤트만 리스너 추가하는 메서드
+    /// </summary>
+    /// <typeparam name="TDto"></typeparam>
+    /// <param name="eventName"></param>
+    /// <param name="listener"></param>
+    public void AddListener<TDto>(Enum eventName, UnityAction<TDto> listener)
+    {
+        _onModelChangedDTO[eventName] = obj =>
+        {
+            if (obj is TDto typed)
+                listener(typed);
+        };
+    }
+
+
+    /// <summary>
+    /// 모델 내 등록 된 이벤트 리스너를 제거하는 메서드
+    /// </summary>
+    /// <param name="listener">제거 할 리스너</param>
+    public void RemoveListener(UnityAction listener)
+    {
+        _onModelChanged.RemoveListener(listener);
+    }
+
+    /// <summary>
+    /// DTO 리스너를 개별적으로 해제 할 수 있다.
+    /// </summary>
+    /// <param name="eventName"></param>
+    public void RemoveListener(Enum eventName)
+    {
+        if (_onModelChangedDTO.ContainsKey(eventName))
+        {
+            _onModelChangedDTO.Remove(eventName);
+        }
+    }
+
+    /// <summary>
+    /// 모델 내 등록 된 모든 리스너를 제거하는 메서드
+    /// </summary>
+    public void ClearListener()
+    {
+        _onModelChanged.RemoveAllListeners();
+        _onModelChangedDTO.Clear();
+    }
+
+    /// <summary>
+    /// 모델 내 등록 된 이벤트를 발생 시키는 메서드
+    /// </summary>
+    public void TriggerEvent()
+    {
+        _onModelChanged?.Invoke();
+    }
+
+    /// <summary>
+    /// DTO 기반 이벤트를 발생 시키는 메서드
+    /// </summary>
+    /// <typeparam name="TDto"></typeparam>
+    /// <param name="eventName"></param>
+    /// <param name="dto"></param>
+    public void TriggerEvent<TDto>(Enum eventName, TDto dto)
+    {
+        if (_onModelChangedDTO.TryGetValue(eventName, out var action))
+        {
+            action.Invoke(dto);
+        }
+    }
+}
---
+++ Assets/Scripts/Global Managers and Controllers/Manager/UI/Base/ViewBase.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public abstract class ViewBase<TModel> : MonoBehaviour where TModel : ModelBase
+{
+    private UnityAction<Enum> _onTriggerAction;
+
+    /// <summary>
+    /// View의 초기화를 진행하는 메서드
+    /// </summary>
+    public virtual void Initialize(PresenterBase<TModel> presenter)
+    {
+        _onTriggerAction += presenter.TriggerEvent;
+    }
+
+    /// <summary>
+    /// View를 활성화 시키는 메서드
+    /// </summary>
+    public virtual void ShowView()
+    {
+        gameObject.SetActive(true);
+    }
+
+    /// <summary>
+    /// View를 비활성화 시키는 메서드
+    /// </summary>
+    public virtual void HideView()
+    {
+        gameObject.SetActive(false);
+    }
+
+    protected void AddUIListener<T, TDto>(T uiElement, Enum eventType, UnityAction<Enum, TDto> callback)
+    where T : UIBehaviour, IEventSystemHandler
+    {
+        AddUIListener(uiElement, (TDto val) => callback?.Invoke(eventType, val));
+    }
+
+    private void AddUIListener<T, TDto>(
+    T uiElement,
+    UnityAction<TDto> listener)
+    where T : UIBehaviour, IEventSystemHandler
+    {
+        switch (uiElement)
+        {
+            case Button button when typeof(TDto) == typeof(Unit):
+                button.onClick.RemoveAllListeners();
+                button.onClick.AddListener(() => listener?.Invoke(default));
+                break;
+
+            case Slider slider when typeof(TDto) == typeof(float):
+                slider.onValueChanged.RemoveAllListeners();
+                slider.onValueChanged.AddListener((UnityAction<float>)(object)listener);
+                break;
+
+            case Toggle toggle when typeof(TDto) == typeof(bool):
+                toggle.onValueChanged.RemoveAllListeners();
+                toggle.onValueChanged.AddListener((UnityAction<bool>)(object)listener);
+                break;
+
+            case Dropdown dropdown when typeof(TDto) == typeof(int):
+                dropdown.onValueChanged.RemoveAllListeners();
+                dropdown.onValueChanged.AddListener((UnityAction<int>)(object)listener);
+                break;
+
+            default:
+                Debug.LogWarning($"[AddUIListener] Unsupported UI type: {typeof(T)}");
+                break;
+        }
+    }
+
+    public abstract void UpdateView(TModel model);
+
+    public abstract void UpdateView<TDto>(TDto dto);
+}
