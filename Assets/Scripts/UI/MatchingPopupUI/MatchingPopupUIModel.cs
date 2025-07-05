a2939fdcd47c801c5a79f94e8a66a46cff8d361d
정하은 <he3080@naver.com>
2025-07-05 23:35:31 +0900

    [Feat] 첫 게임 화면 매칭을 잡아주는 UI MVP 구현

---
+++ Assets/Scripts/UI/MatchingPopupUI/MatchingPopupUIModel.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MatchingEvent
+{
+    StartMatching,
+}
+
+
+public class MatchingPopupUIModel : ModelBase
+{
+    private bool _matchBtnInteract = false;
+
+    public bool MatchBtnInteract => _matchBtnInteract;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+    }
+
+    public void OnMatchingStartBtn()
+    {
+        GameManager.Photon.StartRandomMatch();
+        _matchBtnInteract = false;
+
+        TriggerEvent(MatchingEvent.StartMatching, _matchBtnInteract);
+    }
+
+    public void OnPhaseChanged(GamePhase phase)
+    {
+        bool canStart = (phase == GamePhase.Lobby);
+        TriggerEvent(MatchingEvent.StartMatching, canStart);
+
+        Debug.Log($"GamePhase에 따른 버튼 활성화 비활성화 변경 => 활성화: {canStart}");
+    }
+}
---
+++ Assets/Scripts/UI/MatchingPopupUI/MatchingPopupUIPresenter.cs
@@ -0,0 +1,37 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MatchingPopupUIPresenter : PresenterBase<MatchingPopupUIModel>
+{
+
+    protected override void Initialize()
+    {
+        GameManager.UI.FindAllPresenter();
+
+        base.Initialize();
+
+        AddListener<Unit>(MatchingEvent.StartMatching, _ =>
+        {
+            OnClickMatchingStartBtn();
+        });
+
+        model.AddListener<bool>(MatchingEvent.StartMatching, updateInteract =>
+        {
+            view.UpdateView(updateInteract);
+        });
+    }
+
+    public void OnClickMatchingStartBtn()
+    {
+        model.OnMatchingStartBtn();
+    }
+
+    public void OnPhaseChanged(GamePhase phase)
+    {
+        model.OnPhaseChanged(phase);
+    }
+
+}
