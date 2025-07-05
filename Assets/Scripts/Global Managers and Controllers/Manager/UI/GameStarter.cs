68ba7ed4daf23a5ded092e82de797cf3d6513ed4
정하은 <he3080@naver.com>
2025-07-05 23:32:37 +0900

    [Feat] GameStarter 로 모든 게임 매니저를 초기화 하는 스크립트 추가

---
+++ Assets/Scripts/Global Managers and Controllers/Manager/UI/GameStarter.cs
@@ -0,0 +1,13 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameStarter : MonoBehaviour
+{
+    void Start()
+    {
+        GameManager.Initialize();
+    }
+}
