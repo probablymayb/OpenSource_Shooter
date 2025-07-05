79f08d172aeb7d300784b5a9e983763ff99bfcb2
정하은 <he3080@naver.com>
2025-07-05 23:35:31 +0900

    [Feat] 첫 게임 화면 매칭을 잡아주는 UI MVP 구현

---
+++ Assets/Scripts/UI/MatchingPopupUI/MatchingPopupUIView.cs
@@ -0,0 +1,44 @@
+﻿using Photon.Pun;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MatchingPopupUIView : ViewBase<MatchingPopupUIModel>
+{
+    [SerializeField] private Button matchStartBtn;
+
+    public override void Initialize(PresenterBase<MatchingPopupUIModel> presenter)
+    {
+        base.Initialize(presenter);
+
+        AddUIListener<Button, Unit>(matchStartBtn, MatchingEvent.StartMatching, (evt, _) =>
+        {
+            Debug.Log("Presenter 이벤트 호출됨");
+            presenter.TriggerEvent(evt);
+        });
+    }
+
+    /// <summary>
+    /// 초기 데이터(model)로 View 업데이트
+    /// </summary>
+    /// <param name="model"></param>
+    public override void UpdateView(MatchingPopupUIModel model)
+    {
+        matchStartBtn.interactable = model.MatchBtnInteract;
+    }
+
+    public override void UpdateView<TDto>(TDto dto)
+    {
+        switch (dto)
+        {
+            case bool canStartMatch:
+                Debug.Log($"실제 View 버튼에 활성화 여부 적용: {canStartMatch}");
+                matchStartBtn.interactable = canStartMatch;
+                break;
+
+            default:
+                Debug.LogWarning($"[UpdateView] 처리되지 않은 타입: {typeof(TDto)}");
+                break;
+        }
+    }
+
+}
